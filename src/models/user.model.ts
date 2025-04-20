046ecbb4b6dee5006f9403b3eff20739f50123f7
ahmadibni <ahmadibni007@gmail.com>
2025-04-20 23:30:06 +0700

    add lohin auth

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -62,6 +62,14 @@ UserSchema.pre("save", function (next) {
   next();
 });
 
+UserSchema.methods.toJSON = function () {
+  const user = this.toObject();
+  
+  delete user.password;
+
+  return user;
+}
+
 const UserModel = mongoose.model<User>("User", UserSchema);
 
 export default UserModel;
