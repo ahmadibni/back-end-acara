a18af863b56fb1f6b7e726eafda1bd8715fef7d3
ahmadibni <ahmadibni007@gmail.com>
2025-04-26 15:06:53 +0700

    create generate token

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,3 +1,29 @@
-export const generateToken = () => {
-    
+import { Types } from "mongoose";
+import { User } from "../models/user.model";
+import jwt from "jsonwebtoken";
+import { SECRET } from "./env";
+
+interface IUserToken
+  extends Omit<
+    User,
+    | "password"
+    | "activationCode"
+    | "email"
+    | "username"
+    | "fullName"
+    | "profilePicture"
+  > {
+  id?: Types.ObjectId;
 }
+
+export const generateToken = (user: IUserToken): string => {
+  const token = jwt.sign(user, SECRET, {
+    expiresIn: "1h",
+  });
+
+  return token;
+};
+
+export const generateUserData = (token: string) => {
+    const user = jwt.verify(token, SECRET) as IUserToken;
+};
