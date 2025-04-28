0ac27ae05b102c49e5f1010039c79a75ea42ea59
ahmadibni <ahmadibni007@gmail.com>
2025-04-28 17:09:23 +0700

    edit swagger and add tsconfig

---
+++ src/docs/swagger.ts
@@ -0,0 +1,38 @@
+import swaggerAutogen from "swagger-autogen";
+
+const outputFile = "./swagger_output.json";
+const endpointsFiles = ["../routes/api.ts"];
+
+const doc = {
+    info: {
+        version: "v0.0.1",
+        title: "Dokumentasi API Acara",
+        description: "Dokumentasi API Acara",
+    },
+    servers: [
+        {
+            url: "http://localhost:3000/api",
+            description: "Local Server"
+        },
+        {
+            url: "https://back-end-acara-jet.vercel.app/api",
+            description: "Production Server"
+        }
+    ],
+    components: {
+        securitySchemes: {
+            bearerAuth: {
+                type: "http",
+                scheme: "bearer",
+            }
+        },
+        schemas: {
+            LoginRequest: {
+                identifier: "ahmadibni",
+                password: "12345",
+            }
+        }
+    }
+}
+
+swaggerAutogen({ openapi: "3.0.0"})(outputFile, endpointsFiles, doc);
