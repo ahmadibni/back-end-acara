75407587eb0a98591b14b1a1dc8d81828cd4dd9b
ahmadibni <ahmadibni007@gmail.com>
2025-04-30 17:06:58 +0700

    add env for nodemailer

---
+++ src/utils/mail/mail.ts
@@ -0,0 +1,16 @@
+import nodemailer from "nodemailer";
+import {
+  EMAIL_SMTP_HOST,
+  EMAIL_SMTP_PASS,
+  EMAIL_SMTP_PORT,
+  EMAIL_SMTP_SECURE,
+  EMAIL_SMTP_SERVICE_NAME,
+  EMAIL_SMTP_USER,
+} from "../env";
+
+// const transporter = nodemailer.createTransport({
+//   service: "",
+//   host: "",
+//   port: "",
+//   secure: "",
+// });
